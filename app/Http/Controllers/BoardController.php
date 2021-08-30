2b4479ccce574d19860600159b34707e8ebc784d
Masatoshi <msts.oo0131@gmail.com>
2021-08-31 00:34:36 +0900

    API_Route_create

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Board;
 use Illuminate\Http\Request;
 
 class BoardController extends Controller
 {
     public function index()
     {
-
+      return Board::all();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/tasks', 'TaskController@index');
+Route::get('/board', 'App\Http\Controllers\BoardController@index');
