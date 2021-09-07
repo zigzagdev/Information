9f6a35dc25e7311b9cf8fa603d1d86b5be8c71a5
Masatoshi <msts.oo0131@gmail.com>
2021-09-07 23:01:22 +0900

    Footer_create_display:center_dont_effect_it

--- board/src/components/Footer.js
+++ board/src/components/Footer.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { makeStyles } from "@material-ui/core";
-
-
-const useStyles = makeStyles({
-    footer: {
-        color: "yellow",
-        backgroundColor: "#0f2350",
-        height: "85px",
-        width: "100%",
-        position: "absolute",
-        bottom: 0
-    },
-});
+import React from "react";
+import {Box, Container, Row, Column, FooterLink, Heading } from "./FooterStyles";
 
 const Footer = () => {
-    const classes = useStyles();
-    return <div className={classes.footer}>Footer</div>
+    return (
+        <Box>
+            <Container>
+                <Row>
+                    <Column>
+                        <Heading>About Us</Heading>
+                        <FooterLink href="#">Aim</FooterLink>
+                        <FooterLink href="#">Vision</FooterLink>
+                        <FooterLink href="#">Testimonials</FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Services</Heading>
+                        <FooterLink href="#">Writing</FooterLink>
+                        <FooterLink href="#">Internships</FooterLink>
+                        <FooterLink href="#">Coding</FooterLink>
+                        <FooterLink href="#">Teaching</FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Contact Us</Heading>
+                        <FooterLink href="#">Uttar Pradesh</FooterLink>
+                        <FooterLink href="#">Ahemdabad</FooterLink>
+                        <FooterLink href="#">Indore</FooterLink>
+                        <FooterLink href="#">Mumbai</FooterLink>
+                    </Column>
+                </Row>
+            </Container>
+        </Box>
+    );
 };
-
 export default Footer;
