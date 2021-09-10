ea439556d3ad0b91c69144c68981a01b624417f7
Masatoshi <msts.oo0131@gmail.com>
2021-09-10 19:45:25 +0900

    axios_Import_learning_Button

--- board/src/components/API.js
+++ board/src/components/API.js
@@ -1,11 +1,12 @@
 import React,{ useState,useEffect } from 'react';
 import axios from 'axios';
+import {Button} from "@material-ui/core";
 
 
 const baseURL = "http://127.0.0.1:8000/api/boards";
 
 
-export default function App() {
+export default function API() {
     const [boards, setBoards] = useState([]);
 
     useEffect(() => {
@@ -19,9 +20,12 @@ export default function App() {
         <div>
             <ul>
                 {
-                    boards.map(board => <li key={board.id}> {board.title}{board.name}{board.description} </li>)
+                    boards.map(board => <li key={board.id}> {board.title} </li>)
                 }
             </ul>
+            <Button variant="contained" color="primary" disableElevation>
+                <a href={"http://127.0.0.1:8000/api/boards"}>Push here</a>
+            </Button>
         </div>
     );
 
