fe948d3a199c387470dbdf191abc1acbcaa1a38e
Masatoshi <msts.oo0131@gmail.com>
2021-10-13 20:25:01 +0900

    Header_Paht&_button_changed

--- board/src/App.js
+++ board/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import API_Detail from "./components/API_Detail"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router,Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import API_Index from "./components/API_Index";
 
@@ -14,18 +14,10 @@ function App() {
             <Route exact path="/">
                 <API_Index/>
             </Route>
-            {/*<Switch>*/}
-            {/*<Route exact path="/">*/}
-            {/*    <Notes />*/}
-            {/*</Route>*/}
-            {/*<Route path="/create">*/}
-            {/*    <Create />*/}
-            {/*</Route>*/}
             <Route path="/detail">
                 <API_Detail />
             </Route>
             <Footer/>
-            {/*</Switch>*/}
         </Router>
     );
 }
--- board/src/components/Header.js
+++ board/src/components/Header.js
@@ -3,8 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom'
+
+const style = {
+     color:"white",
+     marginLeft: "1000px"
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -22,12 +26,14 @@ export default function DenseAppBar() {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit">
+                    <Typography variant="h6" color="inherit" style={{textAlign:"left",float:"left"}}>
                         About the new news !
                     </Typography>
+                    <Link to="/create"
+                    style= {style}
+                    >
+                      Create
+                    </Link>
                 </Toolbar>
             </AppBar>
         </div>
